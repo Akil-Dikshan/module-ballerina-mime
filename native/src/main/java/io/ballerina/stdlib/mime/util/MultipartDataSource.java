a6a90e617f778e8c2101adc6bde5528813ed2be4
Danesh Kuruppu <dknkuruppu@gmail.com>
2026-05-28 18:38:04 +0530

    Pass exception object to log.error for full stack trace logging

--- native/src/main/java/io/ballerina/stdlib/mime/util/MultipartDataSource.java
+++ native/src/main/java/io/ballerina/stdlib/mime/util/MultipartDataSource.java
@@ -113,12 +113,12 @@ public class MultipartDataSource implements BRefValue {
             }
             writeFinalBoundaryString(writer, parentBoundaryString);
         } catch (IOException e) {
-            log.error("Error occurred while writing body parts to outputstream", e.getMessage());
+            log.error("Error occurred while writing body parts to outputstream", e);
         } finally {
             try {
                 writer.flush();
             } catch (IOException e) {
-                log.error("Error occurred while flushing multipart writer", e.getMessage());
+                log.error("Error occurred while flushing multipart writer", e);
             }
         }
     }
